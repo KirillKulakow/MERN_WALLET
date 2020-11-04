38800f8f6844a52425c2a82b7dab41725cf9a60f
KirillKulakow <kirillkulakow31@gmail.com>
2020-11-04 19:09:05 +0200

    fix cors

--- api/server.js
+++ api/server.js
@@ -37,18 +37,13 @@ exports.AuthServer = class {
 
   initMiddleware() {
     this.app.use(express.json());
-    this.app.use(
-      cors({
-        origin: [
-          'http://localhost:3000/',
-          'http://localhost:3000/login',
-          'http://localhost:3000/registration',
-          'https://wallet-fp-28.netlify.app',
-          'https://wallet-fp-28.netlify.app/auth/login',
-          'https://wallet-fp-28.netlify.app/auth/register',
-        ],
-      }),
-    );
+    this.app.use((req, res, next) => {
+      res.header("Access-Control-Allow-Origin", '*');
+      res.header("Access-Control-Allow-Credentials", true);
+      res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+      res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+      next();
+    });
   }
 
   initRoutes() {
